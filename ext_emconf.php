7d2a4d00576e972042187722d3b10876e0d4c69f
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2022-05-31 14:00:08 +0200

    [RELEASE] Releasing v1.0.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 $EM_CONF['showpageeditors'] = [
     'title' => 'ShowPageEditors',
     'description' => 'Retrieve every editor who can see a given PID in the backend',
-    'version' => '1.0.0',
+    'version' => '1.0.6',
     'category' => 'misc',
     'author' => 'J. Peter M. Schuler',
     'author_email' => 'j.peter.m.schuler@uni-due.de',
@@ -13,7 +13,7 @@ $EM_CONF['showpageeditors'] = [
     'clearCacheOnLoad' => 0,
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-12.9.99',
+            'typo3' => '9.5.0-11.5.99',
         ],
     ]
 ];
--- tools/release.sh
+++ tools/release.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+gsed -i "s/    'version' => .*/    'version' => '$1',/g" ext_emconf.php
 git commit -a -m "[RELEASE] Releasing v$1";
 git tag v$1  -m "[RELEASE] Releasing v$1";
 git archive -o "showpageeditors_$1.zip" v$1;
