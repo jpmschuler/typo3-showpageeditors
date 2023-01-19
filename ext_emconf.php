8e36a4b739fbce012a1b1c721699152aaf1f3422
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2023-01-19 16:48:19 +0100

    [RELEASE] Releasing v1.0.8

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 $EM_CONF['showpageeditors'] = [
     'title' => 'ShowPageEditors',
     'description' => 'CLI command to retrieve every editor who can see a given PID in the backend',
-    'version' => '1.0.7',
+    'version' => '1.0.8',
     'category' => 'misc',
     'author' => 'J. Peter M. Schuler',
     'author_email' => 'j.peter.m.schuler@uni-due.de',
